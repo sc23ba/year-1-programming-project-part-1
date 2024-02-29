428f74c3d807077e846c116546af3dd669364fad
sc23ba <146708327+sc23ba@users.noreply.github.com>
2024-02-29 16:25:19 +0000

    finishing off

--- testing1.sh
+++ testing1.sh
@@ -6,15 +6,15 @@ echo -e "Testing File Input and validation"
 
 echo "Maze1" | ./first > tmp
 
-if grep -q "Invalid file name, please try again\n" tmp;
+if grep -q "Invalid file name, please try again\n" tmp;  #this test checks that the file can filter out an incorrect filename
 then
     echo "PASS"
 else
     echo "FAIL"
 fi
 
-echo "Maze1.txt" | ./first > tmp
-
+echo "Maze1.txt" | ./first > tmp  #this test then checks that the program can validate a valid file and then not only load the file
+                                  #as an array but also load the starting position of the starting position 
 if grep -q "Select Option\n" tmp; 
 then
     echo "PASS"
@@ -25,16 +25,16 @@ fi
 echo "2" | ./first > tmp
 
 echo "User will need to determine if it is a pass or fail by observing if there is a outputted map and if there is no visible 'S'"
-
+#checks that not only can the program output the file, id validates the claim that it correctly loaded the file and the starting position
 echo "1" | ./first > tmp
 
-if grep -q "Please enter your movement(W/A/S/D): " tmp;
+if grep -q "Please enter your movement(W/A/S/D): " tmp; 
 then 
     echo "PASS"
 else 
     echo "FAIL"
 fi
-
+# checks that the program can tell when a move is invalid and accepts the valid inputs and perfroms their direction correctly 
 echo "W" | ./first > tmp
 if grep -q "Invalid movement, please try again.\n" tmp;
 then 
@@ -65,29 +65,29 @@ echo "S" | ./first > tmp
 echo "1" | ./first > tmp
 echo "S" | ./first > tmp
 
-echo "2" | ./first > tmp
-if grep -q "##########
+echo "2" | ./first > tmp  #this validates the current location function and shows that the player has moved as intended
+if grep -q "########## 
             #S       #
             #   #### #
             #     #  #
             # ####   #
             #      EX#
             ##########" tmp;
-then 
+then   
     echo "PASS"
 else 
     echo "FAIL"
 fi
 
 echo "W" | ./first > tmp
-
+# this checks that the program finishes when intended and only activated on it's intended cell type
 if grep -q "Well dont, you won!" tmp;
 then
    echo "PASS"
 else
    echo "FAIL"
 fi
-
+git
 
 rm -f tmp first
 
